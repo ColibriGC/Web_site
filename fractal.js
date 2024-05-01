4069e32e1671d73e3a02a08711b771a62023606b
ColibriGC <g.dragonis98@gmail.com>
2024-05-01 12:04:46 +0200

    change the coordinate of the text

--- fractal.js
+++ fractal.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw() {
   background(105,105,105);
-  text('Un arbre fractal', 100, 100)
+  text('Un arbre fractal', width/2, height/2)
   translate(width / 2, height / 1.3);
 
   branch(100);
